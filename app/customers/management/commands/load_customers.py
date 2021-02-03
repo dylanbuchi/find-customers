eae453e5f1c7ea1d3179210d7d14a44c85e99606
Dylan Buchi <devybuchi@gmail.com>
2021-02-03 14:46:12 -0300

    add django management command:  load_customers

---
+++ app/customers/management/commands/load_customers.py
@@ -0,0 +1,37 @@
+import csv
+
+from django.core.management import BaseCommand
+
+from app.customers.models import Customer
+
+
+class Command(BaseCommand):
+    help = "Load the customers CSV file and upload it to the database use --path option to type the file path"
+
+    def add_arguments(self, parser):
+        # add the --path option for the command to set the csv file path on the command line
+        parser.add_argument('--path', type=str)
+
+    def handle(self, *args, **kwargs):
+        """Load the customers CSV file and upload it to the database using the Customer model"""
+
+        path = kwargs['path']
+
+        with open(path) as customers_csv_file:
+            fieldnames = [
+                'id', 'first_name', 'last_name', 'email', 'gender', 'company',
+                'city', 'title'
+            ]
+            customers_data = csv.DictReader(customers_csv_file,
+                                            fieldnames=fieldnames)
+            # skip the headers
+            next(customers_data, None)
+
+            for row in customers_data:
+                Customer(first_name=row['first_name'],
+                         last_name=row['last_name'],
+                         email=row['email'],
+                         gender=row['gender'],
+                         company=row['company'],
+                         city=row['city'],
+                         title=row['title']).save()
