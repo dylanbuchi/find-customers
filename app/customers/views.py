396d75f930845d7766fbc1859dccea5986b95e1f
Dylan Buchi <devybuchi@gmail.com>
2021-02-03 14:47:35 -0300

    add CustomerView and CustomerSerializer

--- app/customers/views.py
+++ app/customers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
 
 # Create your views here.
+
+from app.customers.models import Customer
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class CustomerView(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
