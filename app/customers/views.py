77342a30bbd61c705d1b62bc9e2675745f4fb164
Dylan Buchi <devybuchi@gmail.com>
2021-02-04 11:37:55 -0300

    add new method to CustomerView

--- app/customers/views.py
+++ app/customers/views.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers, viewsets
-
-# Create your views here.
+from rest_framework.response import Response
 
 from app.customers.models import Customer
 
@@ -9,8 +8,16 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+        depth = 1
 
 
-class CustomerView(viewsets.ModelViewSet):
+class CustomerView(viewsets.ReadOnlyModelViewSet):
+
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    def get_customer_by_id(self, request, *args, **kwargs):
+        """Returns the customer data by his ID number"""
+
+        customer_data = self.get_serializer(self.get_object().id).data
+        return Response(customer_data)
