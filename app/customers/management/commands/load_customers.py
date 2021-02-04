91158be235d444a8a8430c0adf906b40854ac8b5
Dylan Buchi <devybuchi@gmail.com>
2021-02-04 06:45:01 -0300

    handle integrity error in load_customers

--- app/customers/management/commands/load_customers.py
+++ app/customers/management/commands/load_customers.py
@@ -1,8 +1,9 @@
 import csv
 
+from django.db import IntegrityError
 from django.core.management import BaseCommand
 
-from app.customers.models import Customer, CustomerLocation
+from app.customers.models import Customer, CustomerLocationHandler
 
 
 class Command(BaseCommand):
@@ -17,27 +18,38 @@ class Command(BaseCommand):
 
         path = kwargs['path']
 
-        with open(path) as customers_csv_file:
-            fieldnames = [
-                'id', 'first_name', 'last_name', 'email', 'gender', 'company',
-                'city', 'title'
-            ]
-            customers_data = csv.DictReader(customers_csv_file,
-                                            fieldnames=fieldnames)
-            # skip the headers
-            next(customers_data, None)
+        try:
+            with open(path) as customers_csv_file:
+                fieldnames = [
+                    'id', 'first_name', 'last_name', 'email', 'gender',
+                    'company', 'city', 'title'
+                ]
+                customers_data = csv.DictReader(customers_csv_file,
+                                                fieldnames=fieldnames)
+                # skip the headers
+                next(customers_data, None)
 
-            for row in customers_data:
-                Customer(first_name=row['first_name'],
-                         last_name=row['last_name'],
-                         email=row['email'],
-                         gender=row['gender'],
-                         company=row['company'],
-                         city=row['city'],
-                         title=row['title']).save()
-        self.set_customers_location_coordinates()
+                for row in customers_data:
+                    Customer(first_name=row['first_name'],
+                             last_name=row['last_name'],
+                             email=row['email'],
+                             gender=row['gender'],
+                             company=row['company'],
+                             city=row['city'],
+                             title=row['title']).save()
+
+        except IntegrityError:
+            return (
+                "Customers can't have the same email, please check your data!")
+
+        first_customer = Customer.objects.get(id=1)
+
+        # If The first customer in the file has not coordinates
+        # this means it's the first file load, so we only do it once!
+        if first_customer.latitude is None:
+            self.set_customers_location_coordinates()
 
     def set_customers_location_coordinates(self):
         """After creating the database we set the latitude and longitude fields"""
-        customer_location = CustomerLocation()
+        customer_location = CustomerLocationHandler()
         customer_location.set_customers_locations()
