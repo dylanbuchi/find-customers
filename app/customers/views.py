13d15fc9c1277d5c72cf4fec99435e4740b375db
Dylan Buchi <devybuchi@gmail.com>
2021-02-04 22:37:20 -0300

    update customers views

--- app/customers/views.py
+++ app/customers/views.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers, viewsets
-from rest_framework.response import Response
 
+from rest_framework.response import Response
 from app.customers.models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    """Serializer for the Customer model to translate the model data in other formats"""
     class Meta:
         model = Customer
         fields = '__all__'
@@ -12,12 +13,13 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 
 class CustomerView(viewsets.ReadOnlyModelViewSet):
+    """CustomerView view for the Customer model for the API"""
 
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
     def get_customer_by_id(self, request, *args, **kwargs):
-        """Returns the customer data by his ID number"""
+        """Returns the customer data by his unique id number"""
 
         customer_data = self.get_serializer(self.get_object().id).data
         return Response(customer_data)
